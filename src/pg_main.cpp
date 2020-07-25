ab588d65fa2c1a88ef4d4c8581f820e9ebd17727
Dave <davemers@iu.edu>
2020-07-24 23:13:08 -0400

    adding project files

---
+++ src/pg_main.cpp
@@ -0,0 +1,86 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#if defined(_WIN32) | defined(__WIN32__) | defined(__WIN32) | defined(_WIN64) | defined(__WIN64)
+#include <windows.h>
+//#include "win_network_fcns.h"
+//#include <winsock2.h>
+//#include <iphlpapi.h>
+//
+//#pragma comment(lib, "IPHLPAPI.lib")    // Link with Iphlpapi.lib
+
+#endif
+
+
+
+// C/C++ includes
+#include <cmath>
+#include <cstdint>
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <chrono>
+#include <string>
+#include <vector>
+#include <array>
+#include <algorithm>
+#include <type_traits>
+#include <list>
+
+
+// OpenCV includes
+#include <opencv2/core.hpp>           
+#include <opencv2/highgui.hpp>     
+#include <opencv2/imgproc.hpp> 
+#include <opencv2/video.hpp>
+#include <opencv2/imgcodecs.hpp>
+
+// custom includes
+
+
+
+
+
+// ----------------------------------------------------------------------------------------
+int main(int argc, char** argv)
+{
+    std::string sdate, stime;
+
+    uint64_t idx=0, jdx=0;
+    uint64_t r, c;
+
+    typedef std::chrono::duration<double> d_sec;
+    auto start_time = std::chrono::system_clock::now();
+    auto stop_time = std::chrono::system_clock::now();
+    unsigned long training_duration = 1;  // number of hours to train 
+    auto elapsed_time = std::chrono::duration_cast<d_sec>(stop_time - start_time);
+
+    // do work here
+    try
+    {
+        // this will have to be adjusted based on where/how you are running the code... It should work for VS debugging
+        std::string test_file = "../images/4ZSWD4L.jpg";
+    
+        cv::Mat cv_img = cv::imread(test_file, cv::IMREAD_COLOR);   
+    
+        std::string cv_window = "Original Image";
+        cv::namedWindow(cv_window, cv::WINDOW_NORMAL | cv::WINDOW_KEEPRATIO);
+        cv::imshow(cv_window, cv_img);        
+    
+        cv::waitKey(0);
+    
+    }
+    catch(std::exception e)
+    {
+    
+        std::cout << "Error: " << e.what() << std::endl;
+    }
+    
+    cv::destroyAllWindows();
+    
+    std::cout << "End of Program.  Press Enter to close..." << std::endl;
+	std::cin.ignore();
+    
+}   // end of main
+    

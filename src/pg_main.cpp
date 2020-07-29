f1e493c5fec89b1fa1bd77a2bee091218ae2576b
javier <javic9788@gmail.com>
2020-07-29 09:58:38 -0400

    change path to image

--- src/pg_main.cpp
+++ src/pg_main.cpp
@@ -11,7 +11,6 @@
 #endif
 
 
-
 // C/C++ includes
 #include <cmath>
 #include <cstdint>
@@ -30,16 +29,14 @@
 
 
 // OpenCV includes
-#include <opencv2/core.hpp>           
-#include <opencv2/highgui.hpp>     
-#include <opencv2/imgproc.hpp> 
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
 #include <opencv2/video.hpp>
 #include <opencv2/imgcodecs.hpp>
 
 // custom includes
-
-
-
+#include <file_ops.h>
 
 
 // ----------------------------------------------------------------------------------------
@@ -53,34 +50,34 @@ int main(int argc, char** argv)
     typedef std::chrono::duration<double> d_sec;
     auto start_time = std::chrono::system_clock::now();
     auto stop_time = std::chrono::system_clock::now();
-    unsigned long training_duration = 1;  // number of hours to train 
+    unsigned long training_duration = 1;  // number of hours to train
     auto elapsed_time = std::chrono::duration_cast<d_sec>(stop_time - start_time);
 
     // do work here
     try
     {
         // this will have to be adjusted based on where/how you are running the code... It should work for VS debugging
-        std::string test_file = "../images/4ZSWD4L.jpg";
-    
-        cv::Mat cv_img = cv::imread(test_file, cv::IMREAD_COLOR);   
-    
+        std::string test_file = "C:/Users/Javier/Pictures/4ZSWD4L.jpg";
+
+        cv::Mat cv_img = cv::imread(test_file, cv::IMREAD_COLOR);
+
         std::string cv_window = "Original Image";
         cv::namedWindow(cv_window, cv::WINDOW_NORMAL | cv::WINDOW_KEEPRATIO);
-        cv::imshow(cv_window, cv_img);        
-    
+        cv::imshow(cv_window, cv_img);
+
         cv::waitKey(0);
-    
+
     }
-    catch(std::exception e)
+    catch(std::exception& e)
     {
-    
+
         std::cout << "Error: " << e.what() << std::endl;
     }
-    
+
     cv::destroyAllWindows();
-    
+
     std::cout << "End of Program.  Press Enter to close..." << std::endl;
 	std::cin.ignore();
-    
+
 }   // end of main
-    
+

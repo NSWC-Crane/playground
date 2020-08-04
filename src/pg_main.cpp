55dc203749563e7a673fe97cd45eae08c4cbf64d
javier <javic9788@gmail.com>
2020-08-04 08:20:01 -0400

    updated blurred filter

--- src/pg_main.cpp
+++ src/pg_main.cpp
@@ -42,29 +42,28 @@
 // ----------------------------------------------------------------------------------------
 int main(int argc, char** argv)
 {
-    cv::Mat dst;
-    cv::Mat kernel;
+    cv::Mat blurred_img;
+    cv::Mat mask_img;
+    cv::Mat cv_img;
 
     // initialize parameters for filter2D
     cv::Point anchor = cv::Point(-1, -1);
-    int kernel_size = 3;
+    int kernel_size = 5;
     double delta = 0;
     int ddepth = -1;
 
     // define filter
-    kernel = cv::Mat::ones(kernel_size, kernel_size, CV_32F) * (-1);
-    kernel.at<float>(1, 1) = 12;
-    auto kernel_sum = cv::sum(kernel)[0];
-    kernel = kernel / (float)kernel_sum;
-
+    cv::Mat kernel = cv::Mat::ones(kernel_size, kernel_size, CV_32F)/(float)(kernel_size*kernel_size);
+    
     // do work here
     try
     {
         // this will have to be adjusted based on where/how you are running the code... It should work for VS debugging
         std::string test_file = "C:/Users/Javier/Documents/Projects/playground/images/4ZSWD4L.jpg";
 
-        cv::Mat cv_img = cv::imread(test_file, cv::IMREAD_COLOR);
-
+        // read image as grayscale
+        cv_img = cv::imread(test_file, cv::IMREAD_GRAYSCALE);
+        
         std::string cv_window = "Original Image";
         cv::namedWindow(cv_window, cv::WINDOW_NORMAL | cv::WINDOW_KEEPRATIO);
         cv::imshow(cv_window, cv_img);
@@ -73,12 +72,12 @@ int main(int argc, char** argv)
         std::cout << std::endl << "kernel = " << std::endl << " " << kernel << std::endl << std::endl;
 
         cv_window = "Filtered Image";
-        cv::filter2D(cv_img, dst, ddepth, kernel, anchor, delta , cv::BORDER_DEFAULT);
-        cv::imshow(cv_window, dst);
+        cv::filter2D(cv_img, mask_img, ddepth, kernel, anchor, delta , cv::BORDER_DEFAULT);
+        cv::imshow(cv_window, mask_img);
         cv::waitKey(0);
 
         // save new image
-        cv::imwrite("C:/Users/Javier/Documents/Projects/playground/images/4ZSWD4L_filter2d.jpg", dst);
+        cv::imwrite("C:/Users/Javier/Documents/Projects/playground/images/4ZSWD4L_blur.jpg", mask_img);
 
     }
     catch(std::exception& e)

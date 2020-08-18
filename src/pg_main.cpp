98455263483d2e15913c6007328dbffb2d1a40da
javier <javic9788@gmail.com>
2020-08-18 08:46:05 -0400

    Corrected blurring effect

--- src/pg_main.cpp
+++ src/pg_main.cpp
@@ -88,6 +88,12 @@ void matrix_mult_method(cv::Mat src_img, cv::Mat src_blur, cv::Mat &dst, cv::Mat
 int main(int argc, char** argv)
 {
 
+    // initialize parameters for cv::circle
+    cv::Point point = cv::Point(600, 200);
+    int raduis = rand() % 50 + 100;
+    cv::Scalar color = cv::Scalar(0.45, 0.65, 0.34);
+    int thickness = -1;
+
     // this will have to be adjusted based on where/how you are running the code... It should work for VS debugging
     std::string checker_file = "C:/Users/Javier/Documents/Projects/playground/images/checkerboard_10x10.png";
 
@@ -100,48 +106,80 @@ int main(int argc, char** argv)
     try
     {
         cv::Mat checker_img = cv::imread(checker_file, cv::IMREAD_COLOR);
+
+        // convert data type to 16 bit float
+        cv::Mat checkboard_img(600, 800, CV_32FC3);
+        checker_img.convertTo(checkboard_img, CV_32FC3, 1/255.0); // CV_32F are from 0.0 to 1.0
+
+        // apply 5x5 convolution
+        cv::Mat checkboard_blur;
+        cv::boxFilter(checkboard_img, checkboard_blur, -1, cv::Size(5, 5), cv::Point(-1, -1), true); 
+
+        // clone blurred image and overlay shape
+        cv::Mat checkboard_blur_copy = checkboard_blur.clone();
+        cv::circle(checkboard_blur, point, raduis, color, thickness);
+
+
         
-        std::string cv_window = "Checkerboard Image";
+        // create mask 
+        cv::Mat mask;
+        cv::inRange(checkboard_blur, color, color, mask);
+        
+        // convert mask to CV_32F
+        cv::Mat mask_blur, mask_inv_blur;
+        mask.convertTo(mask_blur, CV_32FC1, 1 / 255.0);
+
+        // apply 9x9 convolution
+        cv::boxFilter(mask_blur, mask_blur, -1, cv::Size(9, 9), cv::Point(-1, -1), true);
+
+        // create (1-mask)
+        cv::Scalar ones_scalar = cv::Scalar(1, 1, 1);
+        cv::subtract(ones_scalar, mask_blur, mask_inv_blur);
+
+        // create 3 channel masks for background + foreground
+        cv::Mat background_mask;
+        cv::Mat in_bg[] = { mask_inv_blur, mask_inv_blur, mask_inv_blur };
+        cv::merge(in_bg, 3, background_mask);
+
+        cv::Mat foreground_mask;
+        cv::Mat in_fg[] = { mask_blur, mask_blur, mask_blur };
+        cv::merge(in_fg, 3, foreground_mask);
+
+
+        // apply 9x9 convolution on checkboard img with overlayed shape
+        cv::boxFilter(checkboard_blur, checkboard_blur, -1, cv::Size(9, 9), cv::Point(-1, -1), true);
+
+        // 3C masks and images element wise multiplication
+        cv::Mat background, foreground;
+        cv::multiply(checkboard_blur_copy, background_mask, background);
+        cv::multiply(checkboard_blur, foreground_mask, foreground);
+
+        // display background (5x5 filter) and foreground (9x9 filter) 
+        std::string cv_window = "Background";
         cv::namedWindow(cv_window, cv::WINDOW_NORMAL | cv::WINDOW_KEEPRATIO);
-        cv::imshow(cv_window, checker_img);
+        cv::imshow(cv_window, background);
         cv::waitKey(0);
-        
-        // initialize parameters for cv::circle
-        cv::Point point = cv::Point(600, 200);
-        int raduis = 60;
-        cv::Scalar color = cv::Scalar(255, 155, 140);
-        int thickness = -1;
-        
-        // draw circle on top of image
-        cv::circle(checker_img, point, 100, color, thickness);
 
-        // create masks 
-        cv::Mat mask, mask_inv;
-        cv::inRange(checker_img, color, color, mask);
-        cv::bitwise_not(mask, mask_inv);
-        cv::divide(255, mask, mask, -1);
-        cv::divide(255, mask_inv, mask_inv, -1);
+        cv_window = "Foreground";
+        cv::namedWindow(cv_window, cv::WINDOW_NORMAL | cv::WINDOW_KEEPRATIO);
+        cv::imshow(cv_window, foreground);
+        cv::waitKey(0);
 
-        //cv::Mat out;
-        //cv::boxFilter(mask, out, -1, cv::Size(4, 4), cv::Point(-1, -1) , true); type is unsigned int8
-
-        // initialize parameters for cv::boxFilter
-        cv::Mat filter_mask;
-        int ddepth = -1;
-        int kernel_size = 10;
-        point = cv::Point(-1, -1);
-        cv::Size ksize = cv::Size(3, 3);
-
-        cv::Mat mask_filter, mask_inv_filter;
-        cv::boxFilter(checker_img, mask_filter, ddepth, cv::Size(18, 18), point, true);
-        cv::boxFilter(checker_img, mask_inv_filter, ddepth, cv::Size(5, 5), point, true);
-
-        cv::Mat mat1, mat2;
-        cv::copyTo(mask_filter, mat1, mask);
-        cv::copyTo(mask_inv_filter, mat2, mask_inv);
+        // add both background and foreground
         cv::Mat dst;
-        cv::add(mat1, mat2, dst);
+        cv::add(background, foreground, dst);
 
+        // display combined image
+        cv_window = "Background + Foreground";
+        cv::namedWindow(cv_window, cv::WINDOW_NORMAL | cv::WINDOW_KEEPRATIO);
+        cv::imshow(cv_window, dst);
+        cv::waitKey(0);
+
+        // convert dst image to CV_8UC3
+        cv::multiply(dst, cv::Scalar(1, 1, 1), dst, 255);
+        dst.convertTo(dst, CV_8UC3); // use alpha parameter to scale 
+
+        // save new image
         cv::imwrite("C:/Users/Javier/Documents/Projects/playground/images/checkerboard_blurred.jpg", dst);
     }
     catch(std::exception& e)

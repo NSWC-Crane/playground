9f32d3b370aa573608e00362d9171cbf74688a6b
Dave <davemers@iu.edu>
2020-08-24 06:18:21 -0400

    Update pg_main.cpp

--- src/pg_main.cpp
+++ src/pg_main.cpp
@@ -80,6 +80,64 @@ void matrix_mult_method(cv::Mat src_img, cv::Mat src_blur, cv::Mat &dst, cv::Mat
 }
 
 
+/*
+This function will take a base image and overlay a set of shapes and then blur them according to the sigma value
+
+@param src input image that will be modified and returned.  This image should be CV_32FC3 type
+@param rng random number generator object
+@param sigma value that determines the blur kernel properties
+@param scale optional parameter to determine the size of the object
+*/
+void blur_layer(cv::Mat& src, 
+    cv::RNG& rng,
+    double sigma,
+    double scale = 0.3)
+{
+
+    // get the image dimensions for other uses
+    int nr = src.rows;
+    int nc = src.cols;
+
+    // clone the source image
+    cv::Mat src_clone = src.clone();
+
+    // create the inital blank mask
+    cv::Mat BM_1 = cv::Mat(src.size(), CV_32FC3, cv::Scalar::all(0.0));
+
+    // generate a random color 
+    cv::Scalar C = (1.0 / 255.0) * cv::Scalar(rng.uniform(0, 256), rng.uniform(0, 256), rng.uniform(0, 256));
+
+    // generate a random point within the image using the cv::RNG uniform funtion (use nr,nc)
+
+
+    // generate the random radius values for an ellipse using one of the RNG functions
+    
+
+    // generate a random angle between 0 and 360 degrees for the ellipse using one of the RNG functions
+
+
+    // use the cv::ellipse function and the random points to generate a random ellipse on top of the src_clone image
+
+
+    // use the same points to generate the same ellipse on the mask with color = CV::Scalar(1.0, 1.0, 1.0)
+
+
+    // blur the src_clone image with the overlay and blur the mask image using the sigma value to determine the blur kernel size
+
+
+
+    // multiply the src image times (cv::Scalar(1.0, 1.0, 1.0) - BM_1)
+    cv::Mat L1_1;
+
+    // multiply the src_clone image times BM_1
+    cv::Mat L1_2;
+
+    // set src equal to L1_1 + L1_2
+
+
+}   // end of blur_layer 
+
+
 // ----------------------------------------------------------------------------------------
 int main(int argc, char** argv)
 {
@@ -87,6 +145,8 @@ int main(int argc, char** argv)
     // initialize parameters for cv::circle
     int raduis = rand() % 50 + 55;
     int thickness = -1;
+
+    cv::RNG rng(1234567);
 
     // this will have to be adjusted based on where/how you are running the code... It should work for VS debugging
     std::string checker_file = "C:/Users/Javier/Documents/Projects/playground/images/checkerboard_10x10.png";

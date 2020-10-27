8cd186703ee16620757f1f9a8a3acb657188cc5d
javier <javic9788@gmail.com>
2020-10-27 10:47:51 -0400

    Adding body of read_blur_params

--- include/blur_params.h
+++ include/blur_params.h
@@ -28,6 +28,8 @@ uint16_t min_dm_value = 0;
 uint16_t max_dm_value = br1_table.size()-1;
 
 
+#include "file_parser.h"
+
 void read_blur_params(std::string param_filename, 
     std::vector<uint8_t> &depthmap_values, 
     std::vector<double> &sigma_table, 
@@ -35,13 +37,63 @@ void read_blur_params(std::string param_filename,
     std::vector<uint8_t> &br2_table,
     uint8_t &dataset_type,
     uint32_t &max_dm_num,
-    uint32_t &num_objects
+    uint32_t &num_objects,
     uint32_t &num_images,
     std::string &save_location
 )
 {
-    
-    
+    std::vector<std::vector<std::string>> params;
+    parse_csv_file(param_filename, params);
+
+
+    for (uint64_t idx = 0; idx < params.size(); ++idx)
+    {
+        switch (idx)
+        {
+        case 0:
+            for (uint32_t i = 0; i < params[idx].size(); i++)
+            {
+                depthmap_values.push_back((uint8_t)std::stoi(params[idx][i]));
+            }
+            break;
+        case 1:
+            for (uint32_t i = 0; i < params[idx].size(); i++)
+            {
+                sigma_table.push_back(std::stod(params[idx][i]));
+            }
+            break;
+        case 2:
+            for (uint32_t i = 0; i < params[idx].size(); i++)
+            {
+                br1_table.push_back((uint8_t)std::stod(params[idx][i]));
+            }
+            break;
+        case 3:
+            for (uint32_t i = 0; i < params[idx].size(); i++)
+            {
+                br2_table.push_back((uint8_t)std::stod(params[idx][i]));
+            }
+            break;
+        case 4:
+            dataset_type = (uint8_t)std::stoi(params[idx][0]);
+            break;
+        case 5:
+            max_dm_num = (uint32_t)std::stoi(params[idx][0]);
+            break;
+        case 6:
+            num_objects = (uint32_t)std::stoi(params[idx][0]);
+            break;
+        case 7:
+            num_images = (uint32_t)std::stoi(params[idx][0]);
+            break;
+        case 8:
+            save_location = params[idx][0];
+            break;
+        default:
+            break;
+        }
+    }
+
     
 }   // end of read_blur_params
     

71f6677b08710c90955c3749a2ece274d2331dd4
Campos <1591706280@mil>
2021-02-01 17:11:36 -0500

    Update test.m

--- matlab/src/test.m
+++ matlab/src/test.m
@@ -12,15 +12,15 @@ image_ext = '*.png';
 listing = dir(strcat(folder_path, '/', '*_cm'));
 coc_map = zeros(41, numel(listing));
 
-for idx=1:numel(listing)
-    folder_name = listing(idx).name;
+for r=1:numel(listing)
+    folder_name = listing(r).name;
     
     split_str = strsplit(folder_name, '_');
     dist = str2double(split_str(1));
     distances(end+1) = dist;
     
     sub_folder = dir(strcat(folder_path, '\', folder_name, '\2020*'));
-    [coc_map(:,idx), temp] = get_blurs(strcat(folder_path, '\', folder_name, '\', sub_folder(1).name));
+    [coc_map(:,r), temp] = get_blurs(strcat(folder_path, '\', folder_name, '\', sub_folder(1).name));
     position = position + temp;
 end
 
@@ -33,12 +33,16 @@ fprintf(fileID,'\n');
 fprintf(fileID,'%d,',position);
 fprintf(fileID,'\n');
 
-[r, c] = size(coc_map);
-for idx=1:r
-    fprintf(fileID,'%d,',coc_map(idx,:));
+[rows, cols] = size(coc_map);
+for r=1:rows
+    for c=1:(cols-1)
+        fprintf(fileID,'%d,',coc_map(r,c));
+    end
+    fprintf(fileID,'%d',coc_map(r, cols));
     fprintf(fileID,'\n');
 end
 
+fclose(fileID);
 
 % plot the surface
 figure

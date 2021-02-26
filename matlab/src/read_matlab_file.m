f95821cb50ddd35f543142ad69535e6f4faeb54b
ricardoxy <ricardoxy@icloud.com>
2021-02-26 14:49:57 -0500

    update_camera_setting_still_need_zoom

--- matlab/src/read_matlab_file.m
+++ matlab/src/read_matlab_file.m
@@ -1,32 +1,47 @@
 function [x, y, z, data] = read_matlab_file(filename)
-
-  
-    % open the file read only
-     %file_id = fopen(filename, 'r');
-  %%% load mydata.mat a b c load('mydata.mat','a','b','c')
-    % file_id =  matfile('filename', 'focus' ,'range','zoom','coc_map');
-    %   load filename.mat;
-    file_id = importdata(filename);
-       
-  %  file_id = matfile(filename);
-    % read in the first row of the csv file.  This should be the x values
-%     x  = textscan(file_id, '', 1, ts_args{:});
-%     x = x{1};
-%     x = x(~isnan(x));
-   %%%%%  x = file_id.range(:,:);
-        x = file_id.range;
-    % read in the second line of the csv file.  This should be the y values
-%     y  = textscan(file_id, '', 1, ts_args{:});
-%     y = y{1};
-%     y = y(~isnan(y));
-     y = file_id.focus;
+    
+     file_id = importdata(filename);
+     x = file_id.focus;
+     y = file_id.range;
      z = file_id.zoom ;
+     data = file_id.coc_map; 
+     
+%      size_x = size(x);
+%      size_y = size(y);
+%      size_z = size(z);
+     
+%      x_max_index  = size_x(2);
+%      y_max_index  = size_y(2);
+%      z_max_index  = size_z(2);
+     
+     x_max_index  = numel(x);
+     y_max_index  = numel(y);
+     z_max_index  = numel(z);
+
+     [range, focus, zoom] = size(data);
+     if range ==  x_max_index 
+         x = file_id.range ;
+     else
+         disp( 'range does not match with data')
+     end
+     if focus == y_max_index
+          y = file_id.focus;
+     else
+         disp( 'focus does not match with data')
+     end
+      if zoom == z_max_index
+          z = file_id.zoom;
+     else
+         disp( 'zoom does not match with data')
+     end
+     
+     
     % close the file
 %       fclose(file_id);
 
     % read in the data starting at row 3
   %  data = csvread(filename, 2, 0);
-   data = file_id.coc_map;
+  
 
 
 end

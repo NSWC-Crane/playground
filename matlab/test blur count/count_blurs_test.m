c8415bce615fce5477f854b276621c5fc9de3fb5
Lockridge <1516627391117002@mil>
2023-01-03 08:05:37 -0500

    Updates parameters

--- matlab/test blur count/count_blurs_test.m
+++ matlab/test blur count/count_blurs_test.m
@@ -70,7 +70,7 @@ Tb.Properties.VariableNames = col_label.';
 indT = 1;
 
 %% Iterate through images
-for idx=111:113 %:numel(listing) 
+for idx=1:5 %:numel(listing) 
     fprintf('Image Filename: %s\n', listing(idx).name);
     % Load in an image and get its size
     img_file = fullfile(img_path, '/', listing(idx).name);

4a682bba9ea34dd8ee48fa10fd8dc2e4b88bd992
Emerson <davemers0160@gmail.com>
2023-01-06 15:01:58 -0500

    added additional method for creating smooth curve

--- matlab/test blur count Rev2/CalculateBlurCount.m
+++ matlab/test blur count Rev2/CalculateBlurCount.m
@@ -4,19 +4,32 @@ function [xT, yT, numBlurPix,startPix, stopPix] = CalculateBlurCount(img_line, i
 % average first 1/2 of intv for point one
 % average next 1/2 and 1/2 beyond next point for point two
 
-xT = 1:intv:length(img_line);
-halfI = round(intv/2);
+method = 1;
+switch(method)
+    case 0
+        xT = 1:intv:length(img_line);
+        halfI = round(intv/2);
 
-yT = zeros(round(length(img_line)/intv),1);
+        yT = zeros(round(length(img_line)/intv),1);
 
-yT(1) = mean(img_line(1:halfI));
-ind = halfI+1;
-for i = 2:length(yT)-1
-    yT(i) = mean(img_line(ind:ind+intv-1));
-    ind = ind+intv;
+        yT(1) = mean(img_line(1:halfI));
+        ind = halfI+1;
+        for i = 2:length(yT)-1
+            yT(i) = mean(img_line(ind:ind+intv-1));
+            ind = ind+intv;
+        end
+
+        yT(i+1) = mean(img_line(ind:end));
+
+    case 1
+        kernel = (1/intv) * ones(intv,1);
+        
+        yT = conv(img_line, kernel, 'valid');
+        yT = cat(2, yT(1)*ones(1, floor(intv/2)), yT, yT(end)*ones(1, floor(intv/2)));
+        xT = 1:length(yT);
+        
 end
 
-yT(i+1) = mean(img_line(ind:end));
 
 [~,indyTmx] = max(yT);
 [~,indyTmn] = min(yT);
